44c3e208f81c80fde5e3522dc6898c1f0c39b981
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-01-06 09:49:07 +0600

    read time done

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -9,9 +9,9 @@ const Blogs = () => {
             .then((data) => setBlogs(data));
     }, []);
 
-    const [bookmarkAdd, setBookmarkAdd] = useState(false);
+    // const [bookmarkAdd, setBookmarkAdd] = useState(false);
     const handleBookmarkAdd = () => {
-        setBookmarkAdd(!bookmarkAdd);
+
     };
 
     const [readTime, setReadTime] = useState(0);
@@ -33,11 +33,13 @@ const Blogs = () => {
                     ></Blog>
                 ))}
             </div>
-            <div className="col-span-2 flex flex-col gap-5">
+            <div className="col-span-2 flex flex-col gap-5 h-screen sticky top-0">
                 <div className="border border-[#355834] rounded-lg p-4 text-xl text-center text-[#355834] font-bold">
                     Spent time on read : {readTime} min
                 </div>
-                <div>heellloo world</div>
+                <div className="bg-[#c0ffc0] h-full rounded-lg text-black pl-5 pt-5">
+                    <h3 className="text-3xl font-semibold mb-5">Bookmarked Blogs : 05</h3>
+                </div>
             </div>
         </div>
     );
